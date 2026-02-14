3d4f293570c7622ed8422e3ecb60166a54d5b982
MichaelKaffel <michaeldkaffel@gmail.com>
2026-02-13 20:47:16 -0800

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -4,8 +4,7 @@ const Campsite = require('./models/campsite');
 const url = 'mongodb://127.0.0.1:27017/nucampsite';
 const connect = mongoose.connect(url, {});
 
-connect.then(() => {
-
+ connect.then(() => {
     console.log('Connected correctly to server');
 
     Campsite.create({
@@ -14,10 +13,26 @@ connect.then(() => {
     })
     .then(campsite => {
         console.log(campsite);
-        return Campsite.find();
+
+        return Campsite.findByIdAndUpdate(campsite._id, {
+            $set: { description: 'Updated Test Document' }
+        }, {
+            new: true
+        });
     })
-    .then(campsites => {
-        console.log(campsites);
+    .then(campsite => {
+        console.log(campsite);
+
+        campsite.comments.push({
+            rating: 5,
+            text: 'What a magnificent view!',
+            author: 'Tinus Lorvaldes'
+        });
+
+        return campsite.save();
+    })
+    .then(campsite => {
+        console.log(campsite);
         return Campsite.deleteMany();
     })
     .then(() => {
@@ -27,4 +42,4 @@ connect.then(() => {
         console.log(err);
         mongoose.connection.close();
     });
-});
+ });
