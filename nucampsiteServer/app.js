ec5dcfb05e7aa4479b09815d4c2663c6650a1593
MichaelKaffel <michaeldkaffel@gmail.com>
2026-02-16 16:53:15 -0800

    Express Session Part 2

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -19,7 +19,7 @@ const url = 'mongodb://127.0.0.1:27017/nucampsite';
 const connect = mongoose.connect(url, {});
 
 connect.then(() => console.log('Connected correctly to server'),
-  err => console.log(err)
+    err => console.log(err)
 )
 
 var app = express();
@@ -35,48 +35,34 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}))
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+    console.log(req.session);
 
 
-  if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+    if (!req.session.user) {
+        const err = new Error('You are not authenticated');
+        err.status = 401;
+        return next(err);
 
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === 'admin' && pass === 'password') {
-      req.session.user = 'admin'
-      return next(); //authorized
     } else {
-      const err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
+        if (req.session.user === 'authenticated') {
+            return next()
+        } else {
+            const err = new Error('You are not authenticated');
+            err.status = 401;
+            return next(err);
+        }
     }
-  } else {
-    if (req.session.user === 'admin') {
-      return next()
-    } else {
-      const err = new Error('You are not authenticated');
-      err.status = 401;
-      return next(err);
-    }
-  }
 
 }
 
@@ -84,26 +70,24 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
