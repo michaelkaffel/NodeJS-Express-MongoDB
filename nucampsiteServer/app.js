c7214dd217f6928c0c705be1b97341f92e8c449c
MichaelKaffel <michaeldkaffel@gmail.com>
2026-02-19 21:17:19 -0800

    Basic Authentication

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -16,8 +16,8 @@ const mongoose = require('mongoose');
 const url = 'mongodb://127.0.0.1:27017/nucampsite';
 const connect = mongoose.connect(url, {});
 
-connect.then(() => console.log('Connected correctly to server'), 
-  err => console.log(err)
+connect.then(() => console.log('Connected correctly to server'),
+    err => console.log(err)
 )
 
 var app = express();
@@ -30,6 +30,36 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+
+    console.log(req.headers);
+
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader) {
+        const err = new Error('You are not authenticated');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
+
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    const user = auth[0];
+    const pass = auth[1];
+
+    if (user === 'admin' && pass === 'password') {
+        return next();
+    } else {
+        const err = new Error('You are not authenticated ');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -39,19 +69,19 @@ app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
