fe448732b7c0825e64b91da6400ed8ac7ca0d487
MichaelKaffel <michaeldkaffel@gmail.com>
2026-02-21 22:51:55 -0800

    Assignment completed

--- nucampsiteServer/routes/users.js
+++ nucampsiteServer/routes/users.js
@@ -7,8 +7,12 @@ const router = express.Router();
 
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    User.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => next(err))
 });
 
 router.post('/signup', (req, res) => {
